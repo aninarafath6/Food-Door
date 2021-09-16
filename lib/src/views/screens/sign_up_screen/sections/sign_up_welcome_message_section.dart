466cd5be283f682ad860a12707627d5d9e6ae215
aninarafath6 <arafathanin6.com>
2021-09-16 21:05:59 +0530

    sign up heading section

--- lib/src/views/screens/sign_up_screen/sections/sign_up_welcome_message_section.dart
+++ lib/src/views/screens/sign_up_screen/sections/sign_up_welcome_message_section.dart
@@ -3,38 +3,36 @@ import 'package:food_door/src/constants/app_colors.dart';
 import 'package:food_door/src/utils/dimensions.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class WelcomeMessageSection extends StatelessWidget {
-  const WelcomeMessageSection({Key? key}) : super(key: key);
+class SignUpWelcomeMessageSection extends StatelessWidget {
+  const SignUpWelcomeMessageSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      // message alignment
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // spacer
         context.spacing(height: 4),
         // welcome section
         Text(
-          'Welcome to',
-          // text style
+          'Create Account',
+          // text styling
           style: _textStyle(
             context,
             fontSize: context.getWidth(6),
             fontWeight: FontWeight.w500,
           ),
         ),
+        // spacer
         context.spacing(height: 1),
-        // dis message
         Text(
-          'Enter you Phone number or Email\naddress for sign in. Enjoy you food.',
-          // message style
+          'Enter your Name, Email and Password\nfor sign up.',
           style: _textStyle(context, fontSize: context.getWidth(3.8)),
         ),
       ],
     );
   }
 
+  // text style
   TextStyle _textStyle(BuildContext context,
       {double? fontSize, FontWeight? fontWeight}) {
     return GoogleFonts.poppins(
--- lib/src/views/screens/sign_up_screen/sign_up_screen.dart
+++ lib/src/views/screens/sign_up_screen/sign_up_screen.dart
@@ -3,8 +3,8 @@ import 'package:food_door/src/constants/app_sizes.dart';
 import 'package:food_door/src/views/screens/login_screen/sections/action_section.dart';
 import 'package:food_door/src/views/screens/login_screen/sections/input_action_section.dart';
 import 'package:food_door/src/views/screens/login_screen/sections/input_section.dart';
-import 'package:food_door/src/views/screens/login_screen/sections/welcome_message_section.dart';
 import 'package:food_door/src/views/screens/sign_up_screen/sections/sign_up_app_bar_section.dart';
+import 'package:food_door/src/views/screens/sign_up_screen/sections/sign_up_welcome_message_section.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -16,7 +16,20 @@ class SignUpScreen extends StatelessWidget {
       // app bar area
       appBar: signUpAppBar(context),
       // content area
-     
+      body: Padding(
+        padding:
+            const EdgeInsets.symmetric(horizontal: AppSizes.defaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const <Widget>[
+            // Spacer(), // welcome message section
+            SignUpWelcomeMessageSection(),
+            // Spacer(),
+            // input section
+            // welcome message section
+          ],
+        ),
+      ),
     );
   }
 }
