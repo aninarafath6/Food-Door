d8d7cb61cc57177b4ba32b3aaab0ff475bdcfbff
aninarafath6 <arafathanin6.com>
2021-09-19 15:20:57 +0530

    app bar theme

--- lib/src/views/screens/login_screen/sections/app_bar_section.dart
+++ lib/src/views/screens/login_screen/sections/app_bar_section.dart
@@ -1,29 +1,26 @@
-
-  import 'package:flutter/material.dart';
-import 'package:food_door/src/constants/app_colors.dart';
+import 'package:flutter/material.dart';
 import 'package:food_door/src/utils/navigation.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 AppBar loginAppBar(BuildContext context) {
-    return AppBar(
-      // leading button : back button on the left side of the app bar
-      leading: IconButton(
-        // pop navigation to the previous screen
-        onPressed: () {
-          // go to back screen
-          context.pop();
-        },
-        // splash radius
-        splashRadius: 25,
-        icon: const Icon(
-          // arrow back ios icon
-          Icons.arrow_back_ios,
-        ),
+  return AppBar(
+    // leading button : back button on the left side of the app bar
+    leading: IconButton(
+      // splash radius
+      splashRadius: 25,
+
+      // pop navigation to the previous screen
+      onPressed: () {
+        // go to back screen
+        context.pop();
+      },
+
+      icon: const Icon(
+        // arrow back ios icon
+        Icons.arrow_back_ios,
       ),
-      // title of the app bar
-      title:const  Text(
-        'Sign In',
-     
-      ),
-    );
-  }
+    ),
+
+    // title of the app bar
+    title: const Text('Sign In'),
+  );
+}
